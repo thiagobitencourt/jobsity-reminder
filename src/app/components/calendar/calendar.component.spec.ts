edd165698466cc764510e63c9c2ffd4e22af2e5a
Thiago Bitencourt <thiago.bitencourt@triersistemas.com.br>
2020-09-25 15:41:56 -0300

    test: fix calendar component tests

--- src/app/components/calendar/calendar.component.spec.ts
+++ src/app/components/calendar/calendar.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IsOutOfMonthPipe } from 'src/app/pipes/is-out-of-month.pipe';
+import { IsTodayPipe } from 'src/app/pipes/is-today.pipe';
+import { IsWeekendPipe } from 'src/app/pipes/is-weekend.pipe';
 
 import { CalendarComponent } from './calendar.component';
 
@@ -8,7 +11,7 @@ describe('CalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CalendarComponent]
+      declarations: [IsTodayPipe, IsWeekendPipe, IsOutOfMonthPipe, CalendarComponent]
     })
     .compileComponents();
   });
