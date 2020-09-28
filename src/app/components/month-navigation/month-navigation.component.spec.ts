fb375d3af5ff2c0394b85270aa83efc0df8aeb0a
Thiago Bitencourt <thiago.bitencourt@triersistemas.com.br>
2020-09-28 07:28:17 -0300

    test: implement unit tests for month-navigation component

--- src/app/components/month-navigation/month-navigation.component.spec.ts
+++ src/app/components/month-navigation/month-navigation.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { addMonths, format, subMonths } from 'date-fns';
 
 import { MonthNavigationComponent } from './month-navigation.component';
 
@@ -21,5 +23,37 @@ describe('MonthNavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the current month', () => {
+    const month = new Date();
+    component.month = month;
+    
+    const monthLabel = fixture.debugElement.query(By.css('.month-label'));
+    expect(monthLabel.nativeElement.textContent).toBe(format(month, 'MMMM yyyy'));
+  });
+
+  it('should be able to navigate to the previous month', () => {
+    const month = new Date();
+    component.month = month;
+    component.previousMonth();
+    expect(component.month).toEqual(subMonths(month, 1));
+  });
+
+  it('should be able to navigate to the next month', () => {
+    const month = new Date();
+    component.month = month;
+    component.nextMonth();
+    expect(component.month).toEqual(addMonths(month, 1));
+  });
+
+  it('should emit an event whit the month when the selected month changes', () => {
+    spyOn(component.monthChanges, 'emit');
+    component.month = new Date();
+    
+    component.nextMonth();
+    component.previousMonth();
+    expect(component.monthChanges.emit).toHaveBeenCalledTimes(2);
+    expect(component.monthChanges.emit).toHaveBeenCalledWith(component.month);
   });
 });
