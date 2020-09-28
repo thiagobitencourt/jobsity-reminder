e3dd982df3e614347209b8e90e93d0384b9ba1ef
Thiago Bitencourt <thiago.bitencourt@triersistemas.com.br>
2020-09-28 07:35:39 -0300

    test: implement unit test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,41 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { isSameMonth } from 'date-fns';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
 import { DialogService } from './services/dialog.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  const dialogService = jasmine.createSpyObj('dialogService', ['openReminderDialog']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      providers: [
-        { provide: DialogService, useValue: {}}
-      ],
+      providers: [{ provide: DialogService, useValue: dialogService }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize with the current month', () => {
+    expect(isSameMonth(component.currentMonth, new Date())).toBeTrue();
+  });
+
+  it('should open the dialog to create a new reminder', () => {
+    dialogService.openReminderDialog = jasmine.createSpy('openReminderDialog').and.returnValue(of({}));
+    component.newReminder();
+    expect(dialogService.openReminderDialog).toHaveBeenCalled();
   });
 });
